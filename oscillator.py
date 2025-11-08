bf78b1f88015a4585e8b4e7d8684ed9055c4a660
Olivia Vernetti <overnetti@gmail.com>
2025-11-08 13:17:58 -0800

    Swap pins

--- oscillator.py
+++ oscillator.py
@@ -1,8 +1,8 @@
 from gpiozero import AngularServo
 from time import sleep
 
-# GPIO 17 (physical pin 11)
-servo = AngularServo(17, min_angle=-90, max_angle=90)
+# GPIO 18 (physical pin 12)
+servo = AngularServo(18, min_angle=-90, max_angle=90)
 
 # Move from -90° → +90° and back once
 for angle in range(-90, 91, 30):
