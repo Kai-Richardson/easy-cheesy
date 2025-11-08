243c59967d1612523ad1c140190c3f8cf6c115c3
Olivia Vernetti <overnetti@gmail.com>
2025-11-08 13:22:20 -0800

    Add logging

--- oscillator.py
+++ oscillator.py
@@ -3,15 +3,19 @@ from time import sleep
 
 # GPIO 18 (physical pin 12)
 servo = AngularServo(18, min_angle=-90, max_angle=90)
+print("This is servo: ", servo)
 
 # Move from -90° → +90° and back once
 for angle in range(-90, 91, 30):
     servo.angle = angle
     sleep(0.5)
 
+print("Completed first rotation.")
+
 for angle in range(90, -91, -30):
     servo.angle = angle
     sleep(0.5)
+print("Completed second rotation.")
 
 servo.angle = 0  # center
 sleep(1)
