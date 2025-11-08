a132807a79ba33de9e904963590f45df1f66b45c
Kai Richardson <kai_richardson@trimble.com>
2025-11-08 15:37:36 -0800

    stepper new

--- stepper.py
+++ stepper.py
@@ -1,20 +1,27 @@
-from gpiozero import PhaseEnableMotor
-from time import sleep
+import time
+import board
+from digitalio import DigitalInOut, Direction
 
-# PHASE, ENABLE, PWM
-motor = PhaseEnableMotor(phase=20, enable=21)
-print("This is the motor: ", motor)
+# dir nd step pins as outputs
+DIR = DigitalInOut(board.D20)
+DIR.direction = Direction.OUTPUT
+STEP = DigitalInOut(board.D21)
+STEP.direction = Direction.OUTPUT
 
-# Move forward slowly
-print("Moving forward slowly")
+# microstep mode, default is 1/8 so 8
+# another ex: 1/16 microstep would be 16
+microMode = 8
+# full rotation multiplied by the microstep divider
+steps = 200 * microMode
 
-motor.forward()
-sleep(2)
-
-print("Moving backward slowly")
-# motor.backward(0.1)
-# sleep(2)
-
-print("Stopping motor")
-motor.stop()
-sleep(1)
+while True:
+    # change dir every loop
+    DIR.value = not DIR.value
+    print("Direction:", DIR.value)
+    for x in range(steps):
+        STEP.value = True
+        time.sleep(0.001)
+        STEP.value = False
+        time.sleep(0.001)
+    print("Full rotation complete")
+    time.sleep(1)
