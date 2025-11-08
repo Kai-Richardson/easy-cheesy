a0ade2b37b8fe318ac47029329d1357033cc2481
Kai Richardson <kai_richardson@trimble.com>
2025-11-08 13:58:57 -0800

    try motor

---
+++ stepper.py
@@ -0,0 +1,20 @@
+from gpiozero import PhaseEnableMotor
+from time import sleep
+
+# PHASE, ENABLE, PWM
+motor = PhaseEnableMotor(phase=20, enable=21)
+print("This is the motor: ", motor)
+
+# Move forward slowly
+print("Moving forward slowly")
+
+motor.forward(0.5)
+sleep(2)
+
+print("Moving backward slowly")
+motor.backward(0.5)
+sleep(2)
+
+print("Stopping motor")
+motor.stop()
+sleep(1)
