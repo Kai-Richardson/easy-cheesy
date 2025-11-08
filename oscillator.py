beab7f06901ad8d65e290437420b3ad27f6202bd
Olivia Vernetti <overnetti@gmail.com>
2025-11-08 13:43:02 -0800

    Point to pin 12

--- oscillator.py
+++ oscillator.py
@@ -2,7 +2,7 @@ from gpiozero import AngularServo
 from time import sleep
 
 # GPIO 18 (physical pin 12)
-servo = AngularServo(18, min_angle=-90, max_angle=90)
+servo = AngularServo(12, min_angle=-90, max_angle=90)
 print("This is servo: ", servo)
 
 # Move from -90° → +90° and back once
