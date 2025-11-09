ba01f8096b8686e96bb4e9b52e36b174dfdca9b8
Olivia Vernetti <overnetti@gmail.com>
2025-11-08 17:58:49 -0800

    Update oscillator.py

--- oscillator.py
+++ oscillator.py
@@ -9,8 +9,10 @@ def scrape_cheese():
     then returns servo to neutral position.
     """
     signal = 12
-    servo = AngularServo(signal, min_angle=-90, max_angle=90)
-    servo.angle = -90  # neutral position
+    servo = AngularServo(signal, min_angle=-90, max_angle=90, min_pulse_width=0.0005,  # 0.5ms
+        max_pulse_width=0.0025) 
+    servo.angle = 0
+    sleep(0.5) 
 
     up_angle = 30       # degrees upward
     down_angle = -30    # degrees downward
@@ -24,3 +26,5 @@ def scrape_cheese():
         sleep(oscillation_speed)
         servo.angle = down_angle
         sleep(oscillation_speed)
+    
+    servo.angle = 0
