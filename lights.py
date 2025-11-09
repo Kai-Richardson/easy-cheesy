c8f53afc26b46a42030f94e30952f967a541f9d6
Parushi Sharma <parushi@Parushis-MacBook-Pro.local>
2025-11-08 21:09:11 -0800

    clean up

--- lights.py
+++ lights.py
@@ -1,8 +1,8 @@
 import board
 import neopixel
+import time
 
-
-pixel_pin = board.RX  # Use GPIO 6 (or any valid pin)
+pixel_pin = board.RX
 num_pixels = 9
 
 pixels = neopixel.NeoPixel(pixel_pin, num_pixels, brightness=0.3, auto_write=False)
@@ -10,4 +10,8 @@ pixels[0] = (10, 0, 0)
 pixels[8] = (0, 10, 0)
 pixels.show()
 
+time.sleep(2)
 
+# Proper cleanup
+pixels.deinit()
+del pixels
