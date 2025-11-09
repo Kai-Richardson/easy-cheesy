98056192af4773dfdad600fa061d176493bc7b38
Parushi Sharma <parushi@Parushis-MacBook-Pro.local>
2025-11-08 20:19:41 -0800

    trying out different pin

--- lights.py
+++ lights.py
@@ -1,7 +1,13 @@
 import board
 import neopixel
 
-pixels = neopixel.NeoPixel(board.NEOPIXEL, 10, auto_write=False)
+
+pixel_pin = board.RX  # Use GPIO 6 (or any valid pin)
+num_pixels = 8
+
+pixels = neopixel.NeoPixel(pixel_pin, num_pixels, brightness=0.3, auto_write=False)
 pixels[0] = (10, 0, 0)
 pixels[9] = (0, 10, 0)
 pixels.show()
+
+
