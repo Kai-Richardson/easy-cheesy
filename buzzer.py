452c213596de0a6b2976604b83dd6901011948bc
Olivia Vernetti <overnetti@gmail.com>
2025-11-08 16:29:05 -0800

    Add buzzer test sound

--- buzzer.py
+++ buzzer.py
@@ -1,24 +1,52 @@
 import qwiic_buzzer
-import time
+# import time
 
 buzzer = qwiic_buzzer.QwiicBuzzer()
 
-if not buzzer.is_connected():
-    print("Buzzer not detected. Check I2C wiring.")
-    exit()
+buzzer.sound_effect_1(volume=buzzer.VOLUME_MAX)
 
-buzzer.begin()
-print("Playing tune...")
+# def sound_effect_cheesed(self, volume=buzzer.VOLUME_MAX):
+#     """
+#     Plays a short melody followed by a "you've been cheesed" tone pattern.
+#     """
 
-# Play C4 (262 Hz) for 500 ms
-buzzer.play_tone(262, 500)
-time.sleep(0.5)
+#     melody = [
+#         (self.NOTE_C4, 0.2),
+#         (self.NOTE_D4, 0.2),
+#         (self.NOTE_E4, 0.2),
+#         (self.NOTE_C4, 0.2),
+#         (self.NOTE_E4, 0.2),
+#         (self.NOTE_G4, 0.2),
+#         (self.NOTE_C5, 0.3),
+#     ]
 
-# Play a short melody
-notes = [262, 294, 330, 349, 392, 440, 494, 523]
-for note in notes:
-    buzzer.play_tone(note, 200)
-    time.sleep(0.05)
+#     # Play main melody
+#     for freq, dur in melody:
+#         self.configure(freq, 0, volume)
+#         self.on()
+#         time.sleep(dur)
+#         self.off()
+#         time.sleep(0.05)
 
-buzzer.stop_tone()
-print("Done!")
+#     # Dramatic pause
+#     time.sleep(0.5)
+
+#     # “You’ve been cheesed” = playful up-down siren pattern
+#     for note in range(400, 1800, 100):
+#         self.configure(note, 0, volume)
+#         self.on()
+#         time.sleep(0.03)
+#     for note in range(1800, 400, -120):
+#         self.configure(note, 0, volume)
+#         self.on()
+#         time.sleep(0.03)
+#     self.off()
+
+#     # Short buzz at end
+#     self.configure(self.NOTE_C5, 200, volume)
+#     self.on()
+#     time.sleep(0.2)
+#     self.off()
+
+
+# sound_effect_cheesed()
