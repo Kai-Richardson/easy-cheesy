26473c1405dad8b9e02c851920f6a6be0e2dc4ef
Olivia Vernetti <overnetti@gmail.com>
2025-11-08 19:28:39 -0800

    Update oscillator.py

--- oscillator.py
+++ oscillator.py
@@ -11,8 +11,8 @@ def scrape_cheese():
     signal = 12
     servo = AngularServo(signal, min_pulse_width=0.0005, max_pulse_width=0.0025) 
 
-    up_angle = 90      # degrees upward
-    down_angle = -90    # degrees downward
+    up_angle = 10     # degrees upward
+    down_angle = -10    # degrees downward
     oscillation_speed = 0.99  # seconds between moves
     run_time = 3.0      # total time to oscillate (seconds)
 
