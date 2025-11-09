ac63e064c9cc6806a76d3e9618b65d909be0c3f8
Parushi Sharma <parushi@Parushis-MacBook-Pro.local>
2025-11-08 21:06:46 -0800

    upadating index

--- lights.py
+++ lights.py
@@ -7,7 +7,7 @@ num_pixels = 9
 
 pixels = neopixel.NeoPixel(pixel_pin, num_pixels, brightness=0.3, auto_write=False)
 pixels[0] = (10, 0, 0)
-pixels[9] = (0, 10, 0)
+pixels[8] = (0, 10, 0)
 pixels.show()
 
 
